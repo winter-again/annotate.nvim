5d5c58c73e732bfccd9a5b938400d1d81c10eba6
Andrew Tiu <mt1229@georgetown.edu>
2023-06-09 22:54:32 -0400

    Previewer works but need to properly get the filename and generate entry info in its own func

--- lua/annotate/telescope/_extensions/annotate.lua
+++ lua/annotate/telescope/_extensions/annotate.lua
@@ -13,6 +13,7 @@ local function annotations(opts)
     opts = opts or {}
     local ns = vim.api.nvim_create_namespace('annotate')
     local annots = vim.api.nvim_buf_get_extmarks(0, ns, 0, -1, {})
+    local bufname = vim.api.nvim_buf_get_name(0)
     pickers.new(opts, {
         prompt_title = 'Annotations',
         finder = finders.new_table({
@@ -20,19 +21,24 @@ local function annotations(opts)
             entry_maker = function(entry)
                 local res = 'Line: ' .. entry[2] + 1 .. ' id: ' .. entry[2]
                 return {
-                    value = entry,
+                    value = entry[2] + 1,
                     display = res,
                     ordinal = entry[2],
+                    -- TODO: need path or filename for the qflist_previewer
+                    path = bufname,
                     lnum = entry[2] + 1
                 }
             end
         }),
         sorter = conf.generic_sorter(opts),
+        -- previewer = conf.grep_previewer(opts),
+        previewer = conf.qflist_previewer(opts),
         attach_mappings = function(prompt_bufnr, map)
             actions.select_default:replace(function()
                 actions.close(prompt_bufnr)
                 local selection = action_state.get_selected_entry()
-                vim.api.nvim_command(tostring(selection.lnum))
+                -- vim.api.nvim_command(tostring(selection.lnum))
+                vim.api.nvim_win_set_cursor(0, {selection.lnum, 0})
             end)
             return true
         end
